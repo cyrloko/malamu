0a0ea429193d802e644b6dd901943d4b908b9fbe
cakpo <cyr.loko@yahoo.com>
2023-12-17 00:36:13 +0100

    Init Malamu

---
+++ src/main/java/com/rissslow/malamu/MalamuApplication.java
@@ -0,0 +1,13 @@
+package com.rissslow.malamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalamuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalamuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rissslow/malamu/MalamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rissslow.malamu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalamuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
